f03fe05243dd11c78b911ebcb8960e81ee991807
frrnhfz <192344366+frrnhfz@users.noreply.github.com>
2024-12-22 01:12:39 +0800

    fix: fix install script from variable errors

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
-# Check for the current theme. If it's 'delta-gitster', the theme is probably
-# already installed. Abort the installtion gracefully.
-if [[ $ZSH_THEME == "delta-gitster" ]]; then
-    exit 0
-fi
-
 # Check if ZSH is installed. If not, ask the user to install it first abort the
 # installation.
-if [[ ! command -v zsh > /dev/null ]]; then
+if ! command -v zsh > /dev/null; then
     echo "[delta-gitster] ZSH not found."
     echo "[delta-gitster] You must first install ZSH."
     exit 1
@@ -14,10 +8,17 @@ fi
 
 # Check for the current shell. If it's not ZSH, ask the user to set it to ZSH
 # first then abort the installation.
-if [[ $SHELL != */zsh ]]; then
+if [[ $SHELL != */zsh && ! -f ~/.zshrc ]]; then
     echo "[delta-gitster] ZSH is not the default shell."
     echo "[delta-gitster] You must first set ZSH as the default shell."
 fi
+
+# Check for the current theme. If it's 'delta-gitster', the theme is probably
+# already installed. Abort the installtion gracefully.
+if grep -q "delta-gitster" ~/.zshrc; then
+    exit 0
+fi
+
 
 # Check for Oh My ZSH. If it's not installed, ask the user to install it first
 # then abort the installation.
@@ -30,9 +31,9 @@ fi
 # being executed locally, copy the theme to Oh My ZSH custom theme folder. If
 # not, download it from GitHub and write it to Oh My ZSH custom theme folder.
 if [[ -f ./delta-gitster.zsh-theme ]]; then
-    cp ./delta-gitster.zsh-theme $ZSH_CUSTOM/themes/delta-gitster.zsh-theme
+    cp ./delta-gitster.zsh-theme "$ZSH/custom/themes/delta-gitster.zsh-theme"
 else
     curl \
         https://raw.githubusercontent.com/frrnhfz/delta-gitster/refs/heads/main/delta-gitster.zsh-theme \
-        -o $ZSH_CUSTOM/themes/delta-gitster.zsh-theme
+        -o $ZSH/custom/themes/delta-gitster.zsh-theme
 fi
